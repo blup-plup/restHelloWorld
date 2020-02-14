1edc6f802b534d34674ebe53e97282c6ce114997
yash.sharma2 <yash.sharma2@olacabs.com>
2020-02-14 17:01:13 +0530

    first commit for rest service using spring

---
+++ src/main/java/com/blupplup/restservice/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blupplup.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
